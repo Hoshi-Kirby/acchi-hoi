2faa327977ea9b9a86acda2e0cf184e21a63faaf
Hoshi-Kirby <hoshi.kirby.s@icloud.com>
2026-03-20 00:01:59 +0900

    first pages

---
+++ src/pages/Result.tsx
@@ -0,0 +1,9 @@
+// import React, { useState, useEffect } from "react";
+import "./Pages.css";
+// import { useNavigate } from "react-router-dom";
+
+const Result: React.FC = () => {
+  return <></>;
+};
+
+export default Result;
---
+++ src/pages/Setup.tsx
@@ -0,0 +1,9 @@
+// import React, { useState, useEffect } from "react";
+import "./Pages.css";
+// import { useNavigate } from "react-router-dom";
+
+const Setup: React.FC = () => {
+  return <></>;
+};
+
+export default Setup;
