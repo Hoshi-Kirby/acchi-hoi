bd3f73c34e69cf236d2c669d96536900bd5d172c
Hoshi-Kirby <hoshi.kirby.s@icloud.com>
2026-03-21 14:18:42 +0900

    emptymenu

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,16 +1,49 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Pages.css";
 // import { useNavigate } from "react-router-dom";
+import { useGameStore } from "../zustand";
+import p1w from "../assets/p1wN.png";
+import p1m from "../assets/p1mN.png";
+import p2w from "../assets/p2wN.png";
+import p2m from "../assets/p2mN.png";
+import p3w from "../assets/p3wN.png";
+import p3m from "../assets/p3mN.png";
+import p4w from "../assets/p4wN.png";
+import p4m from "../assets/p4mN.png";
+import menuButton from "../assets/menuButton.png";
+import menuFrame from "../assets/menuFrame.png";
+const pnw = [p1w, p2w, p3w, p4w];
+const pnm = [p1m, p2m, p3m, p4m];
 
 const Play: React.FC = () => {
-  const clickStart = () => {};
+  const playerCount = useGameStore((state) => state.playerCount);
+  const isMaleCharacter = useGameStore((state) => state.isMaleCharacter);
+
+  const [isMenu, setIsMenu] = useState<boolean>(false);
+
+  const clickMenu = () => {
+    setIsMenu(true);
+  };
+
   return (
     <div className="game-container">
       <div className="back"></div>
-      <div className="title-title">あっちむくなホイ！</div>
-      <button className="title-start-button" onClick={clickStart}>
-        Start
-      </button>
+      <img src={menuButton} className="menu-button" onClick={clickMenu} />
+      <div className="play-chara-content">
+        {Array.from({ length: playerCount }).map((_, i) => (
+          <div className="play-chara-packet-content" key={i}>
+            <img
+              src={isMaleCharacter[i] ? pnw[i] : pnm[i]}
+              className="play-image"
+            />
+          </div>
+        ))}
+      </div>
+      {isMenu && (
+        <div>
+          <img src={menuFrame} className="menu-frame" />
+        </div>
+      )}
     </div>
   );
 };
